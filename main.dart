15b8f6db0c4c495d5e336d2d13b9ab0772ba6e3d
KelfinF10S-boop <anamalang762@gmail.com>
2024-11-14 09:22:10 +0700

    V.beta 1.0
    widget pembookingan tempat

--- main.dart
+++ main.dart
@@ -7,78 +7,157 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    Container(
+      color: Color.fromARGB(255, 46, 51, 122),
+    );
+    const String appTitle = ('pemandangan perumahan padat');
+    TextStyle(color: Color.fromARGB(255, 255, 255, 255));
+    Alignment.center;
+    height:
+    100;
+
+    ;
     return MaterialApp(
-      // Root widget
+      title: appTitle,
       home: Scaffold(
         appBar: AppBar(
-          title: const Text('My Home Page'),
+          title: const Text(appTitle),
         ),
-        body: Center(
-          child: Builder(
-            builder: (context) {
-              return Column(
-                children: [
-                  Container(
-                    padding: const EdgeInsets.all(8.0),
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.center,
-                    children: [
-                      FlutterLogo(
-                        size: 50,
-                      ),
-                      const Text(
-                        "Flutter.",
-                        style: TextStyle(fontSize: 32),
-                      ),
-                    ],
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(10),
-                  ),
-                  TextField(
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Email',
+        body: const SingleChildScrollView(
+            child: Column(
+          children: [
+            Image(image: AssetImage("images/gambar.jpg")),
+            TitleSection(
+              name: 'Legacy hut',
+              location: 'Gunung jati, kec.Jabumg',
+            ),
+            ButtonSection(),
+            Padding(padding: EdgeInsets.all(30.10)),
+            Text(
+                ' rumah di bawah langit senja yang berwarna ungu dan oranye lembut. Langit tampak bergradasi dari warna ungu tua di bagian atas ke warna yang lebih terang mendekati horizon, menciptakan suasana tenang dan damai khas waktu matahari terbenam. Awan tipis tampak samar, menambah kedalaman pada pemandangan ini.')
+          ],
+        )),
+      ),
+    );
+  }
+}
+
+class TitleSection extends StatelessWidget {
+  const TitleSection({
+    super.key,
+    required this.name,
+    required this.location,
+  });
+
+  final String name;
+  final String location;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Row(
+        children: [
+          Expanded(
+            /*1*/
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                /*2*/
+                Padding(
+                  padding: const EdgeInsets.only(bottom: 8),
+                  child: Text(
+                    name,
+                    style: const TextStyle(
+                      fontWeight: FontWeight.bold,
                     ),
                   ),
-                  Container(
-                    margin: EdgeInsets.all(10),
+                ),
+                Text(
+                  location,
+                  style: TextStyle(
+                    color: Colors.grey[500],
                   ),
-                  TextField(
-                    decoration: const InputDecoration(
-                      border: OutlineInputBorder(),
-                      labelText: 'Password',
-                    ),
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(10),
-                  ),
-                  const Text(
-                    'forgot password?',
-                    style: TextStyle(color: Colors.blueAccent),
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(10),
-                  ),
-                  ElevatedButton(
-                    style: ElevatedButton.styleFrom(
-                      backgroundColor: const Color.fromARGB(255, 0, 109, 199),
-                    ),
-                    child: const Text('login'),
-                    onPressed: () {
-                      print('button has clicked!');
-                    },
-                  ),
-                  Container(
-                    margin: EdgeInsets.all(380),
-                    child: const Text('new user? create account.'),
-                  ),
-                ],
-              );
-            },
+                ),
+              ],
+            ),
           ),
-        ),
+          /*3*/
+          Icon(
+            Icons.star,
+            color: Colors.red[500],
+          ),
+          const Text('41'),
+          Padding(padding: EdgeInsets.all(30)),
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonSection extends StatelessWidget {
+  const ButtonSection({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final Color color = Theme.of(context).primaryColor;
+    return SizedBox(
+      child: Row(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        children: [
+          Column(
+            children: [
+              IconButton(onPressed: () {}, icon: const Icon(Icons.call)),
+              Text('CALL')
+            ],
+          ),
+          Column(
+            children: [
+              IconButton(onPressed: () {}, icon: const Icon(Icons.near_me)),
+              Text("ROUTE")
+            ],
+          ),
+          Column(
+            children: [
+              IconButton(onPressed: () {}, icon: const Icon(Icons.share)),
+              Text("SHARE"),
+            ],
+          )
+        ],
+      ),
+    );
+  }
+}
+
+class ButtonWithText extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: [
+        IconButton(onPressed: () {}, icon: const Icon(Icons.call)),
+        IconButton(onPressed: () {}, icon: const Icon(Icons.near_me)),
+        IconButton(onPressed: () {}, icon: const Icon(Icons.share)),
+      ],
+      // ···
+    );
+  }
+}
+
+class TextSection extends StatelessWidget {
+  const TextSection({
+    super.key,
+    required this.description,
+  });
+
+  final String description;
+
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(32),
+      child: Text(
+        "Langit tampak bergradasi dari warna ungu tua di bagian atas ke warna yang lebih terang mendekati horizon, ",
+        softWrap: true,
       ),
     );
   }
